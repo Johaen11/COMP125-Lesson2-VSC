7d6f92e420eb926fede0342770e58d5f61f6a0d5
johaen11 <johngnanakumar@gmail.com>
2016-05-18 10:42:05 -0400

    initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,9 @@
+/* main JavaScripts file*/
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";
+    //code goes in here
+    console.log("App Started...")
+    
+})();
